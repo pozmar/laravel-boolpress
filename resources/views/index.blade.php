326ae5f4adcfacda9ee1436da2618347a38efc15
Martina <martina.pozzerle@libero.it>
2021-02-10 23:29:14 +0100

    #create, update, delete only if auth

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,9 +9,11 @@
 </li>
 <a href="{{ route('posts.show', $post->id) }}">Vai al post</a>
 <br>
+@auth
 <a href="{{ route('posts.edit', $post->id) }}"> Aggiorna il post</a>
 <br>
 <a href="{{ route('posts.destroy', $post->id)}}">Elimina</a>
+@endauth
 
 <br>
 @endforeach
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,10 +2,13 @@
 @section('content')
 <h2>{{ $post->title }}</h2>
 <h4>{{ $post->description }}</h4>
+@auth
 <form action="{{ route('posts.destroy', $post->id) }}" method="post">
      @csrf
       @method('delete')
+    
         <button class="btn btn-danger">Elimina Post</button>
 </form>
+@endauth
 <a href="{{ route('posts.index') }}">Torna ai post</a>
 @endsection
